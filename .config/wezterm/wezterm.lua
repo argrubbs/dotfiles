bae592f0f1455e384d4d1f162086db13a04fbf16
argrubbs <argrubbs@users.noreply.github.com>
2025-09-01 08:54:24 -0500

    added eza to zsh, wezterm stuff

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,14 @@
 -- Wezterm API
 local wezterm = require("wezterm")
+local mux = wezterm.mux
 
 -- Wezterm Config object
 local wezconf = wezterm.config_builder()
+
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
 
 wezconf.color_scheme = "rose-pine"
 
